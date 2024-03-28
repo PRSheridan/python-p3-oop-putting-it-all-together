145ec5c9de5c6944139d9a5e0b7a1002c16824fc
Phil <philrsheridan@gmail.com>
2024-03-28 11:32:07 -0400

    passing all tests

--- lib/book.py
+++ lib/book.py
@@ -4,14 +4,6 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
-    
-    @property
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, title):
-        self._title = title
 
     @property
     def page_count(self):
@@ -19,9 +11,10 @@ class Book:
 
     @page_count.setter
     def page_count(self, page_count):
-        if (isinstance(page_count, int)):
+        if isinstance(page_count, int):
             self._page_count = page_count
         else:
-            raise ValueError("page_count must be an integer")
+            print("page_count must be an integer")
     
-        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
